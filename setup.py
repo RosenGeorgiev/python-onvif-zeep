a2e192df00e9b6773da8b7e9858ebc19b8095a23
Rosen Georgiev <rosengeorgiev93@gmail.com>
2020-11-02 10:55:08 +0200

    Update required zeep version to 4.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 version_path = os.path.join(here, 'onvif/version.txt')
 version = open(version_path).read().strip()
 
-requires = ['zeep >= 3.0.0']
+requires = ['zeep >= 4.0.0']
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
@@ -22,10 +22,8 @@ CLASSIFIERS = [
     'Topic :: Multimedia :: Sound/Audio',
     'Topic :: Utilities',
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.8",
 ]
 
 wsdl_files = [os.path.join('wsdl', item) for item in os.listdir('wsdl')]
@@ -40,11 +38,11 @@ setup(
       long_description=open('README.rst', 'r').read(),
       author='Cherish Chen',
       author_email='sinchb128@gmail.com',
-      maintainer='sinchb',
-      maintainer_email='sinchb128@gmail.com',
+      maintainer='Rossen Georgiev',
+      maintainer_email='rosengeorgiev93 at gmail dot com',
       license='MIT',
       keywords=['ONVIF', 'Camera', 'IPC'],
-      url='http://github.com/quatanium/python-onvif',
+      url='http://github.com/RosenGeorgiev/python-onvif-zeep',
       zip_safe=False,
       packages=find_packages(exclude=['docs', 'examples', 'tests']),
       install_requires=requires,
